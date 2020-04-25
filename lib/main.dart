f6c11d618f1384ff38db88bf2b382ca5cee021b5
jimmy-ugb <58155232+jimmy-ugb@users.noreply.github.com>
2020-04-24 23:51:54 -0600

    main, app, pantalla inicio

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:aplicacion_01/src/app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,24 @@
+import 'package:aplicacion_01/src/view/home_view.dart';
+import 'package:flutter/material.dart';
+//import 'usuario.dart';
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Sigec System",
+      theme: ThemeData(
+        buttonBarTheme: ButtonBarThemeData(buttonMinWidth: 20),
+        textTheme: TextTheme(
+          subhead: TextStyle(
+            fontSize: 15,
+          ),
+        ),
+      ),
+      home:HomeView(),
+   
+
+    );
+  }
+}
