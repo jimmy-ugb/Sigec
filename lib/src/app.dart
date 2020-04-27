9ba101ce7ca11d0daa9387ba11e3aad9eb20c98c
jimmy-ugb <58155232+jimmy-ugb@users.noreply.github.com>
2020-04-26 19:52:53 -0600

    cambio d pantalla login y recuperacion pw

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,4 @@
-//import 'package:aplicacion_01/src/view/recuperarpass.dart';
-//import 'package:aplicacion_01/src/view/fechasPagos.dart';
-//import 'package:aplicacion_01/src/view/login_admin.dart';
-//import 'package:aplicacion_01/src/view/fechasPagos.dart';
-//import 'package:aplicacion_01/src/view/home_view.dart';
-import 'package:aplicacion_01/src/view/fechasPagos.dart';
-import 'package:aplicacion_01/src/view/login_admin.dart';
-import 'package:aplicacion_01/src/view/menu.dart';
-import 'package:aplicacion_01/src/view/puestos.dart';
-//import 'package:aplicacion_01/src/view/usuario.dart';
-//import 'package:aplicacion_01/src/view/login_admin.dart';
-//import 'package:aplicacion_01/src/view/usuario.dart';
-//import 'package:aplicacion_01/src/view/fechasPagos.dart';
+import 'package:aplicacion_01/src/view/home_view.dart';
 import 'package:flutter/material.dart';
 //import 'usuario.dart';
 
@@ -30,13 +18,7 @@ class MyApp extends StatelessWidget {
         
       ),
 
-        routes:<String, WidgetBuilder>{
-          "/loginadmin" : (BuildContext context) => LoginAdminView(),
-          "/puestos" : (BuildContext context) => PuestosView(),
-          "/fechasPagos" : (BuildContext context) => FechasPagoView(),
-        },
-
-     home:MenuView(),
+          home:HomeView(),
   
     );
   }
